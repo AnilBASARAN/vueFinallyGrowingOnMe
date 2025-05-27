2e9de0b3e63e72b4126fb16b3eac583ba9e0dbee
AnilBASARAN <nlbsrn@gmail.com>
2025-05-28 02:35:20 +0300

    nicely coming along

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <header>
       <h1>My Friendss</h1>
     </header>
+    <new-friend></new-friend>
     <ul>
       <friend-contact
       v-for="friend in friends"
@@ -21,8 +22,9 @@
 
 <script>
 import FriendContact from "./components/FriendContact.vue"
+import NewFriend from './components/NewFriend.vue';
 export default {
-  components:{FriendContact},
+  components:{FriendContact, NewFriend},
   data() {
     return {
      friends:[
@@ -66,7 +68,9 @@ header {
   padding: 0;
   list-style: none;
 }
-#app li {
+#app li,
+#app form
+{
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -96,4 +100,19 @@ header {
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
 }
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
+}
+
+
 </style>
---
+++ src/components/NewFriend.vue
@@ -0,0 +1,28 @@
+<template>
+  <form>
+    <div>
+    <label>Name</label>
+    <input type="text" />
+    </div>
+    <div>
+    <label>E-Mail</label>
+    <input type="email" />
+    </div>
+    <div>
+    <label>Phone</label>
+    <input type="tel" />
+    </div>
+    <div>
+        <button>Add Contact</button>
+    </div>
+  </form>
+</template>
+
+<script>
+
+export default{
+    
+}
+
+</script>
+
